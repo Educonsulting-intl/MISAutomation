0e546ff64e2a97b779ff6165f75aec3e7621faa5
matanrozen <rozenmatan1989@gmail.com>
2021-12-14 13:24:28 +0200

    1234

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -25,14 +25,13 @@ public class BaseTest {
 			throws MalformedURLException {
 		cap = new DesiredCapabilities();
 		cap.setBrowserName("chrome");
-		cap.setVersion("81.0");
+		cap.setVersion("85.0");
 		cap.setCapability("enableVideo", false);
-		//driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub/"), cap);
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL("http://192.168.18.22:4444/wd/hub"), cap);
+		//WebDriverManager.chromedriver().setup();
+		//driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		//driver.get("https://www.htzone.co.il/item/70395/%D7%98%D7%9C%D7%A4%D7%95%D7%9F-%D7%A1%D7%9C%D7%95%D7%9C%D7%A8%D7%99--Apple--iPhone-13-Pro-128GB--%D7%90%D7%A4%D7%9C/#");
-		driver.get("https://www.htzone.co.il/item/45408/%D7%AA%D7%A0%D7%95%D7%A8-%D7%91%D7%99%D7%9C%D7%93-%D7%90%D7%99%D7%9F-%D7%A0%D7%99%D7%A8%D7%95%D7%A1%D7%98%D7%94-%D7%A0%D7%A4%D7%97-%D7%AA%D7%90-%D7%90%D7%A4%D7%99%D7%99%D7%94-%D7%92%D7%93%D7%95%D7%9C-71-%D7%9C%D7%99%D7%98%D7%A8-BOSCH-%D7%91%D7%95%D7%A9/");
+		driver.get("https://www.htzone.co.il/item/70395/%D7%98%D7%9C%D7%A4%D7%95%D7%9F-%D7%A1%D7%9C%D7%95%D7%9C%D7%A8%D7%99--Apple--iPhone-13-Pro-128GB--%D7%90%D7%A4%D7%9C/#");
 		
 		
 	}
